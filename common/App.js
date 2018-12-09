bc5e3cac7ec50616362cd9e3e87c7fb3406301cf
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-12-09 09:35:12 -0700

    Update copy and form inputs
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- common/App.js
+++ common/App.js
@@ -53,40 +53,64 @@ class App extends Component {
     return (
       <div className="app">
         <main>
-          <h1 className="masthead">Microservice Boilerplate</h1>
+          <h1 className="masthead">Exercise Tracker</h1>
 
           <div className="container">
             <div className="card">
               <h2 className="masthead">User Stories</h2>
               <ol>
                 <li>
-                  The API GET endpoint is <code>GET /api/:input? </code>
+                  I can create a user by posting form data username to{' '}
+                  <code>/api/exercise/new-user</code> and returned will be an
+                  object with username and _id.
                 </li>
                 <li>
-                  The API POST endpoint is <code>POST /api/post </code>
+                  I can get an array of all users by getting{' '}
+                  <code>api/exercise/users</code> with the same info as when
+                  creating a user.
+                </li>
+                <li>
+                  I can add an exercise to any user by posting form data
+                  userId(_id), description, duration, and optionally date to{' '}
+                  <code>/api/exercise/add</code>. If no date supplied it will
+                  use current date. Returned will be the user object with also
+                  the exercise fields added.
+                </li>
+                <li>
+                  I can retrieve a full exercise log of any user by getting{' '}
+                  <code>/api/exercise/log</code> with a parameter of
+                  userId(_id). Return will be the user object with added array
+                  log and count (total exercise count).
+                </li>
+                <li>
+                  I can retrieve part of the log of any user by also passing
+                  along optional parameters of from & to or limit. (Date format
+                  yyyy-mm-dd, limit = int)
                 </li>
               </ol>
 
               <h2 className="masthead">Example Usage</h2>
-              <code className="response">GET /api/hello_world</code>
+              <code className="response">
+                {`GET /api/exercise/log?{userId}[&from][&to][&limit]`}
+              </code>
               <h2 className="masthead">Example Output</h2>
-              <code className="response">{`{"method":"get", "input":"hello_world"}`}</code>
+              <code className="response">{`{"_id":"5c0c64aaa9e30b02b4aa71ea", "description":"Deep Squats", "duration":"5", "date":"2018-12-01"}`}</code>
             </div>
 
+            {/* Get Log Form */}
             <div className="card">
-              <h2 className="masthead">GET Request</h2>
-
+              <h2 className="masthead">Retrieve a users's exercise log</h2>
               <form
                 ref={form => (this.getFormRef = form)}
                 onSubmit={this.handleGetFormSubmit}
               >
+                <code className="response">{`GET /api/exercise/log?`}</code>
                 <code className="response">
                   <label htmlFor="get">
-                    <span>GET /api/</span>
+                    <span>{`{userId}[&from][&to][&limit]`}</span>
                     <input type="text" id="get" name="get" />
                   </label>
                 </code>
-
                 <div className="buttons">
                   <div>
                     <button>Submit</button>
@@ -100,29 +124,70 @@ class App extends Component {
               </form>
             </div>
 
+            {/* New User Form */}
             <div className="card">
-              <h2 className="masthead">POST Request</h2>
-
+              <h2 className="masthead">Create a New User</h2>
               <form
                 ref={form => (this.postFormRef = form)}
                 onSubmit={this.handlePostFormSubmit}
               >
-                <code className="response">POST /api/post</code>
+                <code className="response">POST /api/exercise/new-user</code>
 
                 <code className="response">
-                  <label htmlFor="foo">
-                    <span>foo</span>
-                    <input type="text" id="foo" name="foo" />
+                  <label htmlFor="username">
+                    <span>username</span>
+                    <input type="text" id="username" name="username" />
                   </label>
                 </code>
 
-                <code className="response">
-                  <label htmlFor="bar">
-                    <span>bar</span>
-                    <input type="text" id="bar" name="bar" />
-                  </label>
-                </code>
+                <div className="buttons">
+                  <div>
+                    <button>Submit</button>
+                  </div>
+                  <div>
+                    <button type="button" onClick={this.handlePostFormClear}>
+                      Clear
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
 
+            {/* New Exercise Form  */}
+            <div className="card">
+              <h2 className="masthead">Add exercises</h2>
+              <form
+                ref={form => (this.postFormRef = form)}
+                onSubmit={this.handlePostFormSubmit}
+              >
+                <code className="response">POST /api/exercise/add</code>
+                <code className="response">
+                  {[
+                    { name: '_id', label: 'User ID', required: true },
+                    {
+                      name: 'description',
+                      label: 'Description',
+                      required: true
+                    },
+                    {
+                      name: 'duration',
+                      label: 'Duration (mins)',
+                      required: true
+                    },
+                    {
+                      name: 'date',
+                      label: 'Date (YYYY-MM-DD)',
+                      required: false
+                    }
+                  ].map(field => (
+                    <div key={field.name} className="response input">
+                      <label htmlFor={field.name}>
+                        <span>{field.label}</span>
+                        <input type="text" id={field.name} name={field.name} />
+                      </label>
+                    </div>
+                  ))}
+                </code>
                 <div className="buttons">
                   <div>
                     <button>Submit</button>
